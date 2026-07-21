ac4c06d0b3b985452b76a932906404693020bf2f
jakariya111222 <geo@aivisibilityagency.net>
2026-07-22 00:08:58 +0600

    Reformat app.js for consistency and readability

--- src/app.js
+++ src/app.js
@@ -4,20 +4,13 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 const app = express();
-
 dotenv.config();
-
-
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
-
 app.use(express.json());
 app.use(cookieParser());
-
-
-
 export default app;
